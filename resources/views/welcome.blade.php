e5e688c2ae02b4803bc1373bf54c3f70a669a4fc
kiet.do <kiet.do@pirago.work>
2021-10-21 15:44:11 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!Doctype>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form method="post">
+            @csrf
+            <button type="submit" name="">Install PDF</button>
+        </form>
+    </body>
+</html>
